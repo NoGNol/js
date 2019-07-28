1790c72d21e782d8f881a5d9335f2de71189f889
Asker <asker.kudaev@gmail.com>
2019-07-29 01:22:24 +0300

    start
    start

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const handler = function()
+		{
+			//document.getElementById("click").addEventListener('click', handler);
+
+			const response = fetch("https://api.ipify.org?format=json");
+			response.then(async result=>{
+				result.json().then(data => {
+					console.log(data);
+					document.getElementById('ip').innerHTML = data.ip;
+				});
+			});
+		}
+		const handler2 = async function()
+		{
+			var newElement = document.createElement('div')
+			const response = await fetch("https://api.ipify.org?format=json");
+			const data = await response.json();
+			console.log(data);
+			// document.getElementById('ip').innerHTML = data.ip;
+			newElement.innerHTML = data.ip;
+			document.getElementById('holder').appendChild(newElement);
+		}
+
+		document.getElementById('click').addEventListener('click', handler2);
